9d78a0508608b42c01b256112e5f4c4e7749ec16
sypark <soun997@gmail.com>
2024-03-29 14:24:23 +0900

    initialize project🎉

---
+++ recordit/src/main/java/com/sillim/recordit/RecorditApplication.java
@@ -0,0 +1,13 @@
+package com.sillim.recordit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecorditApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RecorditApplication.class, args);
+	}
+
+}
