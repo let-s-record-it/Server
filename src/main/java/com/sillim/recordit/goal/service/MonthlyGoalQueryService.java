a615ab74b6f8bab7640da62c073b03856e776726
sypark <soun997@gmail.com>
2024-04-03 23:19:14 +0900

    ✨ feature[#26]: MonthlyGoal 조회 기능 구현

---
+++ src/main/java/com/sillim/recordit/goal/service/MonthlyGoalQueryService.java
@@ -0,0 +1,24 @@
+package com.sillim.recordit.goal.service;
+
+import com.sillim.recordit.global.exception.ErrorCode;
+import com.sillim.recordit.goal.domain.MonthlyGoal;
+import com.sillim.recordit.goal.exception.MonthlyGoalQueryException;
+import com.sillim.recordit.goal.repository.MonthlyGoalJpaRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
+@Transactional(readOnly = true)
+public class MonthlyGoalQueryService {
+
+	private final MonthlyGoalJpaRepository monthlyGoalJpaRepository;
+
+	public MonthlyGoal search(final Long monthlyGoalId) {
+
+		return monthlyGoalJpaRepository
+				.findById(monthlyGoalId)
+				.orElseThrow(() -> new MonthlyGoalQueryException(ErrorCode.MONTHLY_GOAL_NOT_FOUND));
+	}
+}
