9138d46afe03ad2c3dc4a07fb4eb258b3ea8fcf2
sypark <soun997@gmail.com>
2024-04-03 17:08:52 +0900

    🔧 chore[#15]: ErrorCode 정의

---
+++ src/main/java/com/sillim/recordit/global/exception/ErrorCode.java
@@ -0,0 +1,21 @@
+package com.sillim.recordit.global.exception;
+
+import lombok.Getter;
+
+@Getter
+public enum ErrorCode {
+
+	// global
+
+	INVALID_ARGUMENT("ERR_GLOBAL_001", "올바르지 않은 값이 전달되었습니다."),
+	REQUEST_NOT_FOUND("ERR_GLOBAL_002", "요청을 찾을 수 없습니다."),
+	UNHANDLED_EXCEPTION("ERR_GLOBAL_999", "예상치 못한 오류가 발생했습니다.");
+
+	private final String code;
+	private final String description;
+
+	ErrorCode(String code, String description) {
+		this.code = code;
+		this.description = description;
+	}
+}
