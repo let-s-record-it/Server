0b2d1a57212670fc9fc88a0b29cfbddb7cda19ac
sypark <soun997@gmail.com>
2024-04-03 22:41:39 +0900

    ✅ test[#4]: MonthlyAddRequest ➡️ MonthlyGoal 매핑 테스트

---
+++ src/test/java/com/sillim/recordit/goal/controller/dto/request/MonthlyAddRequestTest.java
@@ -0,0 +1,34 @@
+package com.sillim.recordit.goal.controller.dto.request;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertAll;
+
+import com.sillim.recordit.goal.domain.Member;
+import com.sillim.recordit.goal.domain.MonthlyGoal;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class MonthlyAddRequestTest {
+
+	@Test
+	@DisplayName("MonthlyAddRequest 엔티티 매핑")
+	void toEntityTest() {
+
+		MonthlyGoalAddRequest request =
+				new MonthlyGoalAddRequest("취뽀하기!", "취업할 때까지 숨 참는다.", 2024, 4, "#83c8ef");
+		Member member = new Member();
+
+		MonthlyGoal monthlyGoal = request.toEntity(member);
+		assertAll(
+				() -> {
+					assertThat(monthlyGoal.getTitle()).isEqualTo(request.title());
+					assertThat(monthlyGoal.getDescription()).isEqualTo(request.description());
+					assertThat(monthlyGoal.getGoalYear()).isEqualTo(request.goalYear());
+					assertThat(monthlyGoal.getGoalMonth()).isEqualTo(request.goalMonth());
+					assertThat(monthlyGoal.getColorHex()).isEqualTo(request.colorHex());
+					assertThat(monthlyGoal.getAchieved()).isEqualTo(false);
+					assertThat(monthlyGoal.getDeleted()).isEqualTo(false);
+					assertThat(monthlyGoal.getMember()).isEqualTo(member);
+				});
+	}
+}
