42144826397232613e0a3a1fb37e60b1d2b73824
sypark <soun997@gmail.com>
2024-04-05 16:56:21 +0900

    ✨ feature[#32]: 월 목표 목록 조회 기능 구현

--- src/main/java/com/sillim/recordit/goal/controller/MonthlyGoalController.java
+++ src/main/java/com/sillim/recordit/goal/controller/MonthlyGoalController.java
@@ -1,16 +1,21 @@
 package com.sillim.recordit.goal.controller;
 
 import com.sillim.recordit.goal.controller.dto.request.MonthlyGoalUpdateRequest;
+import com.sillim.recordit.goal.controller.dto.response.MonthlyGoalListResponse;
+import com.sillim.recordit.goal.service.MonthlyGoalQueryService;
 import com.sillim.recordit.goal.service.MonthlyGoalUpdateService;
 import jakarta.validation.Valid;
+import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -19,6 +24,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class MonthlyGoalController {
 
 	private final MonthlyGoalUpdateService monthlyGoalUpdateService;
+	private final MonthlyGoalQueryService monthlyGoalQueryService;
 
 	// TODO: Security 적용 시 UserDetails 받도록 변경
 	@PostMapping("/months")
@@ -36,5 +42,16 @@ public class MonthlyGoalController {
 
 		monthlyGoalUpdateService.modify(request, monthlyGoalId);
 		return ResponseEntity.noContent().build();
+	}
+
+	// TODO: Security 적용 시 UserDetails 받도록 변경
+	@GetMapping("/months")
+	public ResponseEntity<List<MonthlyGoalListResponse>> monthlyGoalList(
+			@RequestParam Integer goalYear, @RequestParam Integer goalMonth) {
+
+		return ResponseEntity.ok(
+				monthlyGoalQueryService.searchAllByDate(goalYear, goalMonth, 1L).stream()
+						.map(MonthlyGoalListResponse::from)
+						.toList());
 	}
 }
--- src/main/java/com/sillim/recordit/goal/repository/MonthlyGoalJpaRepository.java
+++ src/main/java/com/sillim/recordit/goal/repository/MonthlyGoalJpaRepository.java
@@ -1,8 +1,14 @@
 package com.sillim.recordit.goal.repository;
 
+import com.sillim.recordit.goal.domain.Member;
 import com.sillim.recordit.goal.domain.MonthlyGoal;
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface MonthlyGoalJpaRepository extends JpaRepository<MonthlyGoal, Long> {}
+public interface MonthlyGoalJpaRepository extends JpaRepository<MonthlyGoal, Long> {
+
+	List<MonthlyGoal> findByGoalYearAndGoalMonthAndMember(
+			Integer goalYear, Integer goalMonth, Member member);
+}
