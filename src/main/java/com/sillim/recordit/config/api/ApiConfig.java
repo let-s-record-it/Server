e5c5a5ee21eb5233ddf74e41f8e7de2248b8f5cc
rt3310 <btac3310@gmail.com>
2024-04-08 02:30:22 +0900

    🔧 chore[#16]: restTemplate 빈 설정 추가

--- src/main/java/com/sillim/recordit/config/api/ApiConfig.java
+++ src/main/java/com/sillim/recordit/config/api/ApiConfig.java
@@ -1,2 +1,19 @@
-package com.sillim.recordit.config.api;public class ApiConfig {
+package com.sillim.recordit.config.api;
+
+import java.time.Duration;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestTemplate;
+
+@Configuration
+public class ApiConfig {
+
+    @Bean
+    public RestTemplate restTemplate() {
+        return new RestTemplateBuilder()
+                .setConnectTimeout(Duration.ofSeconds(3))
+                .build();
+    }
+
 }
