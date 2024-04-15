08b3d118f375e6627379dbe4bfbfe0b447a8bd48
sypark <soun997@gmail.com>
2024-04-15 17:11:09 +0900

    ✨ feature[#34]: 월 목표 상세 조회 dto에 id 컬럼 추가

--- src/main/java/com/sillim/recordit/goal/controller/dto/response/MonthlyGoalDetailsResponse.java
+++ src/main/java/com/sillim/recordit/goal/controller/dto/response/MonthlyGoalDetailsResponse.java
@@ -3,11 +3,17 @@ package com.sillim.recordit.goal.controller.dto.response;
 import com.sillim.recordit.goal.domain.MonthlyGoal;
 
 public record MonthlyGoalDetailsResponse(
-		String title, Integer goalYear, Integer goalMonth, String description, String colorHex) {
+		Long id,
+		String title,
+		Integer goalYear,
+		Integer goalMonth,
+		String description,
+		String colorHex) {
 
 	public static MonthlyGoalDetailsResponse from(MonthlyGoal monthlyGoal) {
 
 		return new MonthlyGoalDetailsResponse(
+				monthlyGoal.getId(),
 				monthlyGoal.getTitle(),
 				monthlyGoal.getGoalYear(),
 				monthlyGoal.getGoalMonth(),
