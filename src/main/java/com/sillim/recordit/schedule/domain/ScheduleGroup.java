4d21e3af496c7a4d128a4538d0cff13678062309
rt3310 <btac3310@gmail.com>
2024-04-26 15:23:22 +0900

    🐛 fix[#36]: member에 column name 수정 calender_id -> member_id

--- src/main/java/com/sillim/recordit/schedule/domain/ScheduleGroup.java
+++ src/main/java/com/sillim/recordit/schedule/domain/ScheduleGroup.java
@@ -3,7 +3,6 @@ package com.sillim.recordit.schedule.domain;
 import com.sillim.recordit.calendar.domain.Calendar;
 import com.sillim.recordit.member.domain.Member;
 import jakarta.persistence.*;
-import java.time.LocalDateTime;
 import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
@@ -20,43 +19,19 @@ public class ScheduleGroup {
 	private Long id;
 
 	@Column(nullable = false)
-	private Long isRepeated;
-
-	@Column(nullable = false)
-	@Enumerated(EnumType.STRING)
-	private ScheduleRepetitionType recurrentType;
-
-	@Column(nullable = false)
-	private Integer repetitionPeriod;
-
-	@Column(nullable = false)
-	private LocalDateTime repetitionStartDate;
-
-	@Column(nullable = false)
-	private LocalDateTime repetitionEndDate;
+	private Boolean isRepeated;
 
 	@ManyToOne(fetch = FetchType.LAZY)
 	@JoinColumn(name = "calendar_id")
 	private Calendar calendar;
 
 	@ManyToOne(fetch = FetchType.LAZY)
-	@JoinColumn(name = "calendar_id")
+	@JoinColumn(name = "member_id")
 	private Member member;
 
 	@Builder
-	public ScheduleGroup(
-			Long isRepeated,
-			ScheduleRepetitionType recurrentType,
-			Integer repetitionPeriod,
-			LocalDateTime repetitionStartDate,
-			LocalDateTime repetitionEndDate,
-			Calendar calendar,
-			Member member) {
+	public ScheduleGroup(Boolean isRepeated, Calendar calendar, Member member) {
 		this.isRepeated = isRepeated;
-		this.recurrentType = recurrentType;
-		this.repetitionPeriod = repetitionPeriod;
-		this.repetitionStartDate = repetitionStartDate;
-		this.repetitionEndDate = repetitionEndDate;
 		this.calendar = calendar;
 		this.member = member;
 	}
