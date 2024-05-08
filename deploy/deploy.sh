fa1fc8574212cd9042051eb83424b7ca0d084e97
rt3310 <btac3310@gmail.com>
2024-05-09 02:09:32 +0900

    🔧 chore[#60]: deploy file 수정

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -2,5 +2,5 @@ REPOSITORY=/home/ec2-user/server
 
 cd $REPOSITORY
 
-docker-compose -f docker-compose.yml down --rmi all
-docker-compose -f docker-compose.yml up -d
+docker compose -f docker-compose.yml down --rmi all
+docker compose -f docker-compose.yml up -d
