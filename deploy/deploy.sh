7d02ed96186d76b9b1841a38b347ecb35649e5ae
rt3310 <btac3310@gmail.com>
2024-05-09 01:19:38 +0900

    🔧 chore[#60]: deploy.sh 수정

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -2,5 +2,7 @@ REPOSITORY=/home/ec2-user/
 
 cd $REPOSITORY
 
+sudo curl -L https://github.com/docker/compose/releases/latest/download/docker-compose-$(uname -s)-$(uname -m) -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
 docker-compose -f docker-compose.yml down --rmi all
 docker-compose -f docker-compose.yml up -d
