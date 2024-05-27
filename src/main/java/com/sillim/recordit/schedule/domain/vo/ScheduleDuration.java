69ea5ac6065d2d5c1c9b5f8a94212fe7125f9229
rt3310 <btac3310@gmail.com>
2024-05-27 15:04:14 +0900

    🚑️ hotfix[#76]: 일저 시각 저장 시 0초가 아닌 현재 초가 저장되는 현상 수정

--- src/main/java/com/sillim/recordit/schedule/domain/vo/ScheduleDuration.java
+++ src/main/java/com/sillim/recordit/schedule/domain/vo/ScheduleDuration.java
@@ -17,39 +17,41 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class ScheduleDuration {
 
-	@Column(nullable = false)
-	private Boolean isAllDay;
+    @Column(nullable = false)
+    private Boolean isAllDay;
 
-	@Column(nullable = false)
-	private LocalDateTime startDatetime;
+    @Column(nullable = false)
+    private LocalDateTime startDatetime;
 
-	@Column(nullable = false)
-	private LocalDateTime endDatetime;
+    @Column(nullable = false)
+    private LocalDateTime endDatetime;
 
-	private ScheduleDuration(
-			Boolean isAllDay, LocalDateTime startDatetime, LocalDateTime endDatetime) {
-		validate(startDatetime, endDatetime);
-		this.isAllDay = isAllDay;
-		this.startDatetime = startDatetime;
-		this.endDatetime = endDatetime;
-	}
+    private ScheduleDuration(
+            Boolean isAllDay, LocalDateTime startDatetime, LocalDateTime endDatetime) {
+        validate(startDatetime, endDatetime);
+        this.isAllDay = isAllDay;
+        this.startDatetime = startDatetime;
+        this.endDatetime = endDatetime;
+    }
 
-	public static ScheduleDuration createAllDay(
-			LocalDateTime startDatetime, LocalDateTime endDatetime) {
-		return new ScheduleDuration(
-				true,
-				LocalDateTime.of(startDatetime.toLocalDate(), LocalTime.MIN),
-				LocalDateTime.of(endDatetime.toLocalDate(), LocalTime.MIN));
-	}
+    public static ScheduleDuration createAllDay(
+            LocalDateTime startDatetime, LocalDateTime endDatetime) {
+        return new ScheduleDuration(
+                true,
+                LocalDateTime.of(startDatetime.toLocalDate(), LocalTime.MIN),
+                LocalDateTime.of(endDatetime.toLocalDate(), LocalTime.MIN));
+    }
 
-	public static ScheduleDuration createNotAllDay(
-			LocalDateTime startDatetime, LocalDateTime endDatetime) {
-		return new ScheduleDuration(false, startDatetime, endDatetime);
-	}
+    public static ScheduleDuration createNotAllDay(
+            LocalDateTime startDatetime, LocalDateTime endDatetime) {
+        return new ScheduleDuration(false,
+                startDatetime.withHour(0).withMinute(0).withSecond(0).withNano(0),
+                endDatetime.withHour(0).withMinute(0).withSecond(0).withNano(0));
+    }
 
-	private void validate(LocalDateTime startDatetime, LocalDateTime endDatetime) {
-		if (startDatetime.isAfter(endDatetime)) {
-			throw new InvalidScheduleDurationException(ErrorCode.INVALID_DURATION);
-		}
-	}
+    private void validate(LocalDateTime startDatetime, LocalDateTime endDatetime) {
+        if (startDatetime.isAfter(endDatetime)) {
+            throw new InvalidScheduleDurationException(ErrorCode.INVALID_DURATION);
+        }
+    }
 }
