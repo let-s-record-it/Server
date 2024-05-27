fb3758de1b96064a9c0d169219fffa773ba0b5f5
rt3310 <btac3310@gmail.com>
2024-05-27 15:51:14 +0900

    ✨ feature[#78]: 회원가입 시 기본 캘린더가 생성되는 기능 구현

--- src/main/java/com/sillim/recordit/calendar/dto/request/CalendarAddRequest.java
+++ src/main/java/com/sillim/recordit/calendar/dto/request/CalendarAddRequest.java
@@ -8,7 +8,14 @@ import org.hibernate.validator.constraints.Length;
 public record CalendarAddRequest(
 		@Length(min = 1, max = 30) String title, @ColorHexValid String colorHex) {
 
+	public static final String GENERAL_CALENDAR_TITLE = "일반";
+	public static final String GENERAL_CALENDAR_COLOR_HEX = "ff40d974";
+
 	public Calendar toCalendar(Member member) {
 		return Calendar.builder().title(title).colorHex(colorHex).member(member).build();
+	}
+
+	public static CalendarAddRequest createGeneralCalendar() {
+		return new CalendarAddRequest(GENERAL_CALENDAR_TITLE, GENERAL_CALENDAR_COLOR_HEX);
 	}
 }
