4f95ea822204ee4792330c146630312d6a8a44f6
rt3310 <btac3310@gmail.com>
2024-06-05 14:57:51 +0900

    ✅ test[#93]: 일간 일정 조회 테스트 코드 작성

--- src/test/java/com/sillim/recordit/schedule/repository/CustomScheduleRepositoryTest.java
+++ src/test/java/com/sillim/recordit/schedule/repository/CustomScheduleRepositoryTest.java
@@ -6,9 +6,11 @@ import com.sillim.recordit.calendar.domain.Calendar;
 import com.sillim.recordit.calendar.fixture.CalendarFixture;
 import com.sillim.recordit.member.domain.Member;
 import com.sillim.recordit.member.fixture.MemberFixture;
+import com.sillim.recordit.schedule.domain.RepetitionPattern;
 import com.sillim.recordit.schedule.domain.Schedule;
 import com.sillim.recordit.schedule.domain.ScheduleGroup;
 import com.sillim.recordit.schedule.fixture.ScheduleFixture;
+import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.util.List;
 import org.junit.jupiter.api.BeforeEach;
@@ -24,64 +26,127 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 @DataJpaTest
 class CustomScheduleRepositoryTest {
 
-	@Qualifier("customScheduleRepositoryImpl") @Autowired
-	CustomScheduleRepository customScheduleRepository;
+    @Qualifier("customScheduleRepositoryImpl")
+    @Autowired
+    CustomScheduleRepository customScheduleRepository;
 
-	@Autowired ScheduleRepository scheduleRepository;
-	@Autowired TestEntityManager em;
+    @Autowired
+    ScheduleRepository scheduleRepository;
+    @Autowired
+    TestEntityManager em;
 
-	Member member;
-	Calendar calendar;
-	ScheduleGroup scheduleGroup;
+    Member member;
+    Calendar calendar;
 
-	@BeforeEach
-	void setEntities() {
-		member = em.persist(MemberFixture.DEFAULT.getMember());
-		calendar = em.persist(CalendarFixture.DEFAULT.getCalendar(member));
-		scheduleGroup = em.persist(new ScheduleGroup(false));
-	}
+    @BeforeEach
+    void setEntities() {
+        member = em.persist(MemberFixture.DEFAULT.getMember());
+        calendar = em.persist(CalendarFixture.DEFAULT.getCalendar(member));
+    }
 
-	@Test
-	@DisplayName("년 월에 맞는 일정을 조회한다.")
-	void searchSchedules() {
-		scheduleRepository.save(
-				ScheduleFixture.DEFAULT.getSchedule(
-						scheduleGroup,
-						calendar,
-						LocalDateTime.of(2023, 2, 1, 0, 0),
-						LocalDateTime.of(2024, 1, 1, 0, 0)));
-		Schedule schedule2 =
-				scheduleRepository.save(
-						ScheduleFixture.DEFAULT.getSchedule(
-								scheduleGroup,
-								calendar,
-								LocalDateTime.of(2023, 2, 1, 0, 0),
-								LocalDateTime.of(2024, 2, 1, 0, 0)));
-		Schedule schedule3 =
-				scheduleRepository.save(
-						ScheduleFixture.DEFAULT.getSchedule(
-								scheduleGroup,
-								calendar,
-								LocalDateTime.of(2024, 1, 1, 0, 0),
-								LocalDateTime.of(2025, 1, 1, 0, 0)));
-		Schedule schedule4 =
-				scheduleRepository.save(
-						ScheduleFixture.DEFAULT.getSchedule(
-								scheduleGroup,
-								calendar,
-								LocalDateTime.of(2024, 2, 1, 0, 0),
-								LocalDateTime.of(2025, 1, 1, 0, 0)));
-		scheduleRepository.save(
-				ScheduleFixture.DEFAULT.getSchedule(
-						scheduleGroup,
-						calendar,
-						LocalDateTime.of(2024, 3, 1, 0, 0),
-						LocalDateTime.of(2025, 1, 1, 0, 0)));
+    @Test
+    @DisplayName("년 월에 맞는 일정을 조회한다.")
+    void searchSchedules() {
+        ScheduleGroup scheduleGroup1 = em.persist(new ScheduleGroup(false));
+        ScheduleGroup scheduleGroup2 = em.persist(new ScheduleGroup(false));
+        ScheduleGroup scheduleGroup3 = em.persist(new ScheduleGroup(false));
+        ScheduleGroup scheduleGroup4 = em.persist(new ScheduleGroup(false));
+        ScheduleGroup scheduleGroup5 = em.persist(new ScheduleGroup(false));
+        scheduleRepository.save(
+                ScheduleFixture.DEFAULT.getSchedule(
+                        scheduleGroup1,
+                        calendar,
+                        LocalDateTime.of(2023, 2, 1, 0, 0),
+                        LocalDateTime.of(2024, 1, 1, 0, 0)));
+        Schedule schedule2 =
+                scheduleRepository.save(
+                        ScheduleFixture.DEFAULT.getSchedule(
+                                scheduleGroup2,
+                                calendar,
+                                LocalDateTime.of(2023, 2, 1, 0, 0),
+                                LocalDateTime.of(2024, 2, 1, 0, 0)));
+        Schedule schedule3 =
+                scheduleRepository.save(
+                        ScheduleFixture.DEFAULT.getSchedule(
+                                scheduleGroup3,
+                                calendar,
+                                LocalDateTime.of(2024, 1, 1, 0, 0),
+                                LocalDateTime.of(2025, 1, 1, 0, 0)));
+        Schedule schedule4 =
+                scheduleRepository.save(
+                        ScheduleFixture.DEFAULT.getSchedule(
+                                scheduleGroup4,
+                                calendar,
+                                LocalDateTime.of(2024, 2, 1, 0, 0),
+                                LocalDateTime.of(2025, 1, 1, 0, 0)));
+        scheduleRepository.save(
+                ScheduleFixture.DEFAULT.getSchedule(
+                        scheduleGroup5,
+                        calendar,
+                        LocalDateTime.of(2024, 3, 1, 0, 0),
+                        LocalDateTime.of(2025, 1, 1, 0, 0)));
 
-		List<Schedule> scheduleInMonth =
-				customScheduleRepository.findScheduleInMonth(calendar.getId(), 2024, 2);
+        List<Schedule> scheduleInMonth =
+                customScheduleRepository.findScheduleInMonth(calendar.getId(), 2024, 2);
 
-		assertThat(scheduleInMonth).hasSize(3);
-		assertThat(scheduleInMonth).containsExactlyInAnyOrder(schedule2, schedule3, schedule4);
-	}
+        assertThat(scheduleInMonth).hasSize(3);
+        assertThat(scheduleInMonth).containsExactlyInAnyOrder(schedule2, schedule3, schedule4);
+    }
+
+    @Test
+    @DisplayName("년 월 일에 맞는 일정을 조회한다.")
+    void searchSchedulesInDay() {
+        ScheduleGroup scheduleGroup1 = em.persist(new ScheduleGroup(false));
+        ScheduleGroup scheduleGroup2 = em.persist(new ScheduleGroup(false));
+        ScheduleGroup scheduleGroup3 = em.persist(new ScheduleGroup(false));
+        ScheduleGroup scheduleGroup4 = em.persist(new ScheduleGroup(false));
+        ScheduleGroup scheduleGroup5 = em.persist(new ScheduleGroup(false));
+        em.persist(RepetitionPattern.createDaily(1, LocalDateTime.of(2023, 2, 1, 0, 0),
+                LocalDateTime.of(2023, 3, 1, 0, 0),
+                scheduleGroup2));
+        em.persist(RepetitionPattern.createDaily(1, LocalDateTime.of(2024, 1, 1, 0, 0),
+                LocalDateTime.of(2024, 2, 1, 0, 0),
+                scheduleGroup3));
+        em.persist(RepetitionPattern.createDaily(1, LocalDateTime.of(2024, 2, 1, 0, 0),
+                LocalDateTime.of(2024, 3, 1, 0, 0),
+                scheduleGroup4));
+        scheduleRepository.save(
+                ScheduleFixture.DEFAULT.getSchedule(
+                        scheduleGroup1,
+                        calendar,
+                        LocalDateTime.of(2023, 2, 1, 0, 0),
+                        LocalDateTime.of(2024, 1, 1, 0, 0)));
+        scheduleRepository.save(
+                ScheduleFixture.DEFAULT.getSchedule(
+                        scheduleGroup2,
+                        calendar,
+                        LocalDateTime.of(2023, 2, 1, 0, 0),
+                        LocalDateTime.of(2024, 2, 1, 0, 0)));
+        scheduleRepository.save(
+                ScheduleFixture.DEFAULT.getSchedule(
+                        scheduleGroup3,
+                        calendar,
+                        LocalDateTime.of(2024, 1, 1, 0, 0),
+                        LocalDateTime.of(2024, 2, 1, 23, 59)));
+        scheduleRepository.save(
+                ScheduleFixture.DEFAULT.getSchedule(
+                        scheduleGroup4,
+                        calendar,
+                        LocalDateTime.of(2024, 2, 1, 0, 0),
+                        LocalDateTime.of(2025, 1, 1, 0, 0)));
+        scheduleRepository.save(
+                ScheduleFixture.DEFAULT.getSchedule(
+                        scheduleGroup5,
+                        calendar,
+                        LocalDateTime.of(2024, 3, 1, 0, 0),
+                        LocalDateTime.of(2025, 1, 1, 0, 0)));
+
+        List<Schedule> scheduleInDay =
+                customScheduleRepository.findScheduleInDay(calendar.getId(),
+                        LocalDate.of(2024, 2, 1));
+
+        assertThat(scheduleInDay).hasSize(3);
+        assertThat(scheduleInDay.get(0).getScheduleGroup()).isNotNull();
+        assertThat(scheduleInDay.get(0).getScheduleGroup().getRepetitionPattern()).isNotNull();
+    }
 }
--- src/test/java/com/sillim/recordit/schedule/service/ScheduleQueryServiceTest.java
+++ src/test/java/com/sillim/recordit/schedule/service/ScheduleQueryServiceTest.java
@@ -8,6 +8,7 @@ import static org.mockito.BDDMockito.given;
 import com.sillim.recordit.schedule.domain.Schedule;
 import com.sillim.recordit.schedule.domain.ScheduleGroup;
 import com.sillim.recordit.schedule.repository.ScheduleRepository;
+import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Optional;
@@ -21,60 +22,91 @@ import org.mockito.junit.jupiter.MockitoExtension;
 @ExtendWith(MockitoExtension.class)
 class ScheduleQueryServiceTest {
 
-	@Mock ScheduleRepository scheduleRepository;
-	@InjectMocks ScheduleQueryService scheduleQueryService;
+    @Mock
+    ScheduleRepository scheduleRepository;
+    @InjectMocks
+    ScheduleQueryService scheduleQueryService;
 
-	@Test
-	@DisplayName("schedule id를 통해 일정을 조회할 수 있다.")
-	void searchScheduleByScheduleId() {
-		Schedule expectedSchedule =
-				Schedule.builder()
-						.title("title")
-						.description("description")
-						.isAllDay(false)
-						.startDatetime(LocalDateTime.of(2024, 1, 1, 0, 0))
-						.endDatetime(LocalDateTime.of(2024, 2, 1, 0, 0))
-						.colorHex("aaffbb")
-						.setLocation(true)
-						.place("서울역")
-						.latitude(36.0)
-						.longitude(127.0)
-						.setAlarm(true)
-						.alarmTime(LocalDateTime.of(2024, 1, 1, 0, 0))
-						.scheduleGroup(new ScheduleGroup(false))
-						.build();
-		given(scheduleRepository.findById(anyLong())).willReturn(Optional.of(expectedSchedule));
+    @Test
+    @DisplayName("schedule id를 통해 일정을 조회할 수 있다.")
+    void searchScheduleByScheduleId() {
+        Schedule expectedSchedule =
+                Schedule.builder()
+                        .title("title")
+                        .description("description")
+                        .isAllDay(false)
+                        .startDatetime(LocalDateTime.of(2024, 1, 1, 0, 0))
+                        .endDatetime(LocalDateTime.of(2024, 2, 1, 0, 0))
+                        .colorHex("aaffbb")
+                        .setLocation(true)
+                        .place("서울역")
+                        .latitude(36.0)
+                        .longitude(127.0)
+                        .setAlarm(true)
+                        .alarmTime(LocalDateTime.of(2024, 1, 1, 0, 0))
+                        .scheduleGroup(new ScheduleGroup(false))
+                        .build();
+        given(scheduleRepository.findById(anyLong())).willReturn(Optional.of(expectedSchedule));
 
-		Schedule schedule = scheduleQueryService.searchSchedule(1L);
+        Schedule schedule = scheduleQueryService.searchSchedule(1L);
 
-		assertThat(schedule).isEqualTo(expectedSchedule);
-	}
+        assertThat(schedule).isEqualTo(expectedSchedule);
+    }
 
-	@Test
-	@DisplayName("특정 달의 일정을 조회할 수 있다.")
-	void searchScheduleInMonth() {
-		Schedule expectedSchedule =
-				Schedule.builder()
-						.title("title")
-						.description("description")
-						.isAllDay(false)
-						.startDatetime(LocalDateTime.of(2024, 1, 1, 0, 0))
-						.endDatetime(LocalDateTime.of(2024, 2, 1, 0, 0))
-						.colorHex("aaffbb")
-						.setLocation(true)
-						.place("서울역")
-						.latitude(36.0)
-						.longitude(127.0)
-						.setAlarm(true)
-						.alarmTime(LocalDateTime.of(2024, 1, 1, 0, 0))
-						.scheduleGroup(new ScheduleGroup(false))
-						.build();
-		given(scheduleRepository.findScheduleInMonth(anyLong(), eq(2024), eq(1)))
-				.willReturn(List.of(expectedSchedule));
+    @Test
+    @DisplayName("특정 달의 일정을 조회할 수 있다.")
+    void searchScheduleInMonth() {
+        Schedule expectedSchedule =
+                Schedule.builder()
+                        .title("title")
+                        .description("description")
+                        .isAllDay(false)
+                        .startDatetime(LocalDateTime.of(2024, 1, 1, 0, 0))
+                        .endDatetime(LocalDateTime.of(2024, 2, 1, 0, 0))
+                        .colorHex("aaffbb")
+                        .setLocation(true)
+                        .place("서울역")
+                        .latitude(36.0)
+                        .longitude(127.0)
+                        .setAlarm(true)
+                        .alarmTime(LocalDateTime.of(2024, 1, 1, 0, 0))
+                        .scheduleGroup(new ScheduleGroup(false))
+                        .build();
+        given(scheduleRepository.findScheduleInMonth(anyLong(), eq(2024), eq(1)))
+                .willReturn(List.of(expectedSchedule));
 
-		List<Schedule> schedules = scheduleQueryService.searchSchedulesInMonth(1L, 2024, 1);
+        List<Schedule> schedules = scheduleQueryService.searchSchedulesInMonth(1L, 2024, 1);
 
-		assertThat(schedules).hasSize(1);
-		assertThat(schedules.get(0)).isEqualTo(expectedSchedule);
-	}
+        assertThat(schedules).hasSize(1);
+        assertThat(schedules.get(0)).isEqualTo(expectedSchedule);
+    }
+
+    @Test
+    @DisplayName("특정 일의 일정을 조회할 수 있다.")
+    void searchScheduleInDay() {
+        Schedule expectedSchedule =
+                Schedule.builder()
+                        .title("title")
+                        .description("description")
+                        .isAllDay(false)
+                        .startDatetime(LocalDateTime.of(2024, 1, 1, 0, 0))
+                        .endDatetime(LocalDateTime.of(2024, 2, 1, 0, 0))
+                        .colorHex("aaffbb")
+                        .setLocation(true)
+                        .place("서울역")
+                        .latitude(36.0)
+                        .longitude(127.0)
+                        .setAlarm(true)
+                        .alarmTime(LocalDateTime.of(2024, 1, 1, 0, 0))
+                        .scheduleGroup(new ScheduleGroup(false))
+                        .build();
+        given(scheduleRepository.findScheduleInDay(anyLong(), eq(LocalDate.of(2024, 1, 15))))
+                .willReturn(List.of(expectedSchedule));
+
+        List<Schedule> schedules = scheduleQueryService.searchSchedulesInDay(1L,
+                LocalDate.of(2024, 1, 15));
+
+        assertThat(schedules).hasSize(1);
+        assertThat(schedules.get(0)).isEqualTo(expectedSchedule);
+    }
 }
