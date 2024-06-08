be94da3912946b3da83c60b8571b2c7ee67b0d46
sypark <soun997@gmail.com>
2024-06-09 04:43:16 +0900

    🚚rename[#88]: Weekday, WeekNumber 공통 enum 패키지로 이동

--- src/main/java/com/sillim/recordit/enums/date/WeekNumber.java
+++ src/main/java/com/sillim/recordit/enums/date/WeekNumber.java
@@ -1,6 +1,8 @@
 package com.sillim.recordit.enums.date;
 
 import com.fasterxml.jackson.annotation.JsonValue;
+import java.time.LocalDate;
+import java.time.Period;
 
 public enum WeekNumber {
 	FIRST(1),
@@ -18,5 +20,11 @@ public enum WeekNumber {
 	@JsonValue
 	public Integer getValue() {
 		return value;
+	}
+
+	public boolean contains(final LocalDate date) {
+		LocalDate firstDayOfMonth = date.withDayOfMonth(1);
+		return date.isAfter(firstDayOfMonth.plus(Period.ofDays(7 * (value - 1) - 1)))
+				&& date.isBefore(firstDayOfMonth.plus(Period.ofDays(7 * (value))));
 	}
 }
