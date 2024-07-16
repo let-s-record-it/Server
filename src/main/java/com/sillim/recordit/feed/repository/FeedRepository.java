f079252546f841ffb1ce64e9921b382555528424
rt3310 <btac3310@gmail.com>
2024-07-17 03:40:00 +0900

    ✨ feature[#149]: 피드 상세 조회 기능 구현

--- src/main/java/com/sillim/recordit/feed/repository/FeedRepository.java
+++ src/main/java/com/sillim/recordit/feed/repository/FeedRepository.java
@@ -3,4 +3,4 @@ package com.sillim.recordit.feed.repository;
 import com.sillim.recordit.feed.domain.Feed;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface FeedRepository extends JpaRepository<Feed, Long> {}
+public interface FeedRepository extends JpaRepository<Feed, Long>, CustomFeedRepository {}
