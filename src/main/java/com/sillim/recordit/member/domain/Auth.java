248f0d5fd094639ccac5a4d605c2c8f898cad714
rt3310 <btac3310@gmail.com>
2024-08-27 19:46:58 +0900

    🐛 fix[#191]: OAuthAccount에 @Column 추가

--- src/main/java/com/sillim/recordit/member/domain/Auth.java
+++ src/main/java/com/sillim/recordit/member/domain/Auth.java
@@ -1,5 +1,6 @@
 package com.sillim.recordit.member.domain;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Embeddable;
 import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
@@ -11,6 +12,7 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 public class Auth {
 
+	@Column
 	private String oauthAccount;
 
 	@Enumerated(EnumType.STRING)
