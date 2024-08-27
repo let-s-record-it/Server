4ea4d37f6522487e47d6296f7b02550d177a9c7f
rt3310 <btac3310@gmail.com>
2024-08-28 03:24:16 +0900

    💄 style[#193]: spotless 적용

--- src/test/java/com/sillim/recordit/member/controller/MemberControllerTest.java
+++ src/test/java/com/sillim/recordit/member/controller/MemberControllerTest.java
@@ -1,11 +1,5 @@
 package com.sillim.recordit.member.controller;
 
-import com.sillim.recordit.support.restdocs.RestDocsTest;
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.test.web.servlet.ResultActions;
-
 import static com.sillim.recordit.support.restdocs.ApiDocumentUtils.getDocumentRequest;
 import static com.sillim.recordit.support.restdocs.ApiDocumentUtils.getDocumentResponse;
 import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
@@ -13,17 +7,23 @@ import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuild
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+import com.sillim.recordit.support.restdocs.RestDocsTest;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.web.servlet.ResultActions;
+
 @WebMvcTest(MemberController.class)
 class MemberControllerTest extends RestDocsTest {
 
-    @Test
-    @DisplayName("자신의 정보를 조회한다.")
-    void feedDetails() throws Exception {
-        ResultActions perform = mockMvc.perform(get("/api/v1/members/me"));
+	@Test
+	@DisplayName("자신의 정보를 조회한다.")
+	void feedDetails() throws Exception {
+		ResultActions perform = mockMvc.perform(get("/api/v1/members/me"));
 
-        perform.andExpect(status().isOk());
+		perform.andExpect(status().isOk());
 
-        perform.andDo(print())
-                .andDo(document("my-profile", getDocumentRequest(), getDocumentResponse()));
-    }
+		perform.andDo(print())
+				.andDo(document("my-profile", getDocumentRequest(), getDocumentResponse()));
+	}
 }
