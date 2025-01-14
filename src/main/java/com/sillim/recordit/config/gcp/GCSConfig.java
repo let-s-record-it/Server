a660f780acad251afe0af4e4524eb47820d8cc08
rt3310 <btac3310@gmail.com>
2025-01-15 00:58:02 +0900

    fix[#201]: gradlew test 시 gcs bean 등록 문제 수정

--- src/main/java/com/sillim/recordit/config/gcp/GCSConfig.java
+++ src/main/java/com/sillim/recordit/config/gcp/GCSConfig.java
@@ -8,9 +8,11 @@ import java.io.InputStream;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.util.ResourceUtils;
 
 @Configuration
+@Profile("!test")
 public class GCSConfig {
 
 	@Value("${spring.cloud.gcp.storage.credentials.location}")
