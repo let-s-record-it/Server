ee850d3ddc3b6ad3d529f3fafd81e383b49821a6
rt3310 <btac3310@gmail.com>
2025-01-21 20:46:05 +0900

    feature[#208]: 푸시 알림을 위한 FCM 토큰을 Member에 저장하도록 구현

--- src/main/java/com/sillim/recordit/member/dto/request/LoginRequest.java
+++ src/main/java/com/sillim/recordit/member/dto/request/LoginRequest.java
@@ -2,4 +2,5 @@ package com.sillim.recordit.member.dto.request;
 
 import com.sillim.recordit.member.domain.OAuthProvider;
 
-public record LoginRequest(String idToken, String accessToken, OAuthProvider provider) {}
+public record LoginRequest(
+		String idToken, String accessToken, OAuthProvider provider, String pushAlarmToken) {}
