60014565bf52d3a2116df07148ac9ad243759638
rt3310 <btac3310@gmail.com>
2025-02-08 00:10:10 +0900

    fix[#230]: 캘린더 주인이 아니여도 멤버 목록이 보이도록 수정

--- src/main/java/com/sillim/recordit/calendar/controller/CalendarController.java
+++ src/main/java/com/sillim/recordit/calendar/controller/CalendarController.java
@@ -11,8 +11,10 @@ import com.sillim.recordit.calendar.service.CalendarQueryService;
 import com.sillim.recordit.calendar.service.JoinCalendarService;
 import com.sillim.recordit.config.security.authenticate.CurrentMember;
 import com.sillim.recordit.member.domain.Member;
+
 import java.net.URI;
 import java.util.List;
+
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -28,58 +30,56 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 public class CalendarController {
 
-	private final CalendarQueryService calendarQueryService;
-	private final CalendarCommandService calendarCommandService;
-	private final CalendarMemberService calendarMemberService;
-	private final JoinCalendarService joinCalendarService;
+    private final CalendarQueryService calendarQueryService;
+    private final CalendarCommandService calendarCommandService;
+    private final CalendarMemberService calendarMemberService;
+    private final JoinCalendarService joinCalendarService;
 
-	@GetMapping
-	public ResponseEntity<List<CalendarResponse>> calendarList(@CurrentMember Member member) {
-		return ResponseEntity.ok(
-				calendarMemberService.searchCalendarsByMemberId(member.getId()).stream()
-						.map(CalendarResponse::from)
-						.toList());
-	}
+    @GetMapping
+    public ResponseEntity<List<CalendarResponse>> calendarList(@CurrentMember Member member) {
+        return ResponseEntity.ok(
+                calendarMemberService.searchCalendarsByMemberId(member.getId()).stream()
+                        .map(CalendarResponse::from)
+                        .toList());
+    }
 
-	@PostMapping
-	public ResponseEntity<CalendarResponse> addCalendar(
-			@RequestBody CalendarAddRequest request, @CurrentMember Member member) {
-		Calendar calendar = calendarCommandService.addCalendar(request, member.getId());
-		return ResponseEntity.created(URI.create("/api/v1/calendars/" + calendar.getId()))
-				.body(CalendarResponse.from(calendar));
-	}
+    @PostMapping
+    public ResponseEntity<CalendarResponse> addCalendar(
+            @RequestBody CalendarAddRequest request, @CurrentMember Member member) {
+        Calendar calendar = calendarCommandService.addCalendar(request, member.getId());
+        return ResponseEntity.created(URI.create("/api/v1/calendars/" + calendar.getId()))
+                .body(CalendarResponse.from(calendar));
+    }
 
-	@DeleteMapping("/{calendarId}")
-	public ResponseEntity<Void> calendarDelete(
-			@PathVariable Long calendarId, @CurrentMember Member member) {
-		calendarCommandService.deleteByCalendarId(calendarId, member.getId());
-		return ResponseEntity.noContent().build();
-	}
+    @DeleteMapping("/{calendarId}")
+    public ResponseEntity<Void> calendarDelete(
+            @PathVariable Long calendarId, @CurrentMember Member member) {
+        calendarCommandService.deleteByCalendarId(calendarId, member.getId());
+        return ResponseEntity.noContent().build();
+    }
 
-	@GetMapping("/{calendarId}/members")
-	public ResponseEntity<List<CalendarMemberResponse>> calendarMemberList(
-			@PathVariable Long calendarId, @CurrentMember Member member) {
-		return ResponseEntity.ok(
-				calendarMemberService.searchCalendarMembers(calendarId, member.getId()).stream()
-						.map(CalendarMemberResponse::of)
-						.toList());
-	}
+    @GetMapping("/{calendarId}/members")
+    public ResponseEntity<List<CalendarMemberResponse>> calendarMemberList(@PathVariable Long calendarId) {
+        return ResponseEntity.ok(
+                calendarMemberService.searchCalendarMembers(calendarId).stream()
+                        .map(CalendarMemberResponse::of)
+                        .toList());
+    }
 
-	@GetMapping("/{calendarId}/members/{memberId}")
-	public ResponseEntity<CalendarMemberResponse> calendarMemberDetails(
-			@PathVariable Long calendarId,
-			@PathVariable Long memberId,
-			@CurrentMember Member member) {
-		return ResponseEntity.ok(
-				CalendarMemberResponse.of(
-						calendarMemberService.searchCalendarMember(
-								calendarId, memberId, member.getId())));
-	}
+    @GetMapping("/{calendarId}/members/{memberId}")
+    public ResponseEntity<CalendarMemberResponse> calendarMemberDetails(
+            @PathVariable Long calendarId,
+            @PathVariable Long memberId) {
+        return ResponseEntity.ok(
+                CalendarMemberResponse.of(
+                        calendarMemberService.searchCalendarMember(
+                                calendarId, memberId)));
+    }
 
-	@PostMapping("/join")
-	public ResponseEntity<Void> joinInCalendar(
-			@RequestBody JoinInCalendarRequest request, @CurrentMember Member member) {
-		joinCalendarService.joinInCalendar(request.inviteCode(), member.getId());
-		return ResponseEntity.created(URI.create("")).build();
-	}
+    @PostMapping("/join")
+    public ResponseEntity<Void> joinInCalendar(
+            @RequestBody JoinInCalendarRequest request, @CurrentMember Member member) {
+        joinCalendarService.joinInCalendar(request.inviteCode(), member.getId());
+        return ResponseEntity.created(URI.create("")).build();
+    }
 }
