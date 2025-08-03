9f4b01946bbe64685aaab62a5c3d6a42c7e5f8fe
rt3310 <btac3310@gmail.com>
2025-08-04 01:39:19 +0900

    test[#278]: 스크랩 동시성 이슈 테스트 작성

---
+++ src/test/java/com/sillim/recordit/feed/service/MultipleFeedScrapServiceTest.java
@@ -0,0 +1,66 @@
+package com.sillim.recordit.feed.service;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import com.sillim.recordit.feed.domain.Feed;
+import com.sillim.recordit.feed.dto.response.FeedDetailsResponse;
+import com.sillim.recordit.feed.fixture.FeedFixture;
+import com.sillim.recordit.feed.repository.FeedRepository;
+import com.sillim.recordit.global.lock.RedisLockUtil;
+import com.sillim.recordit.member.domain.Member;
+import com.sillim.recordit.member.fixture.MemberFixture;
+import com.sillim.recordit.member.repository.MemberRepository;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+@ActiveProfiles("test")
+@SpringBootTest
+class MultipleFeedScrapServiceTest {
+
+	@Autowired MemberRepository memberRepository;
+	@Autowired FeedRepository feedRepository;
+
+	@Autowired FeedScrapService feedScrapService;
+	@Autowired FeedQueryService feedQueryService;
+
+	@Test
+	@DisplayName("스크랩을 빠르게 여러 번 눌러도 한 번만 적용된다.")
+	void multipleFeedScrap() throws InterruptedException {
+		Member member = memberRepository.save(MemberFixture.DEFAULT.getMember());
+		Feed feed = feedRepository.save(FeedFixture.DEFAULT.getFeed(member));
+		int threadCount = 100;
+		ExecutorService executorService = Executors.newFixedThreadPool(32);
+		CountDownLatch latch = new CountDownLatch(threadCount);
+
+		for (int i = 0; i < threadCount; i++) {
+			long feedId = feed.getId();
+			String key = "scrap:" + feedId + ":" + member;
+			executorService.submit(
+					() -> {
+						try {
+							RedisLockUtil.acquireAndRunLock(
+									key,
+									() -> {
+										feedScrapService.feedScrap(feedId, member.getId());
+										return true;
+									});
+						} finally {
+							latch.countDown();
+						}
+					});
+		}
+
+		latch.await();
+
+		FeedDetailsResponse feedResponse =
+				feedQueryService.searchById(feed.getId(), member.getId());
+
+		assertThat(feedResponse.isScraped()).isTrue();
+	}
+}
