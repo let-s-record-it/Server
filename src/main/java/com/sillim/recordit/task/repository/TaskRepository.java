ec3e690165f254b4c43fc7a37fb9f0bae62e3881
sypark <soun997@gmail.com>
2025-08-14 05:23:00 +0900

    feature[#291]: task save 로직 batch 처리

--- src/main/java/com/sillim/recordit/task/repository/TaskRepository.java
+++ src/main/java/com/sillim/recordit/task/repository/TaskRepository.java
@@ -10,7 +10,8 @@ import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface TaskRepository extends JpaRepository<Task, Long>, CustomTaskRepository {
+public interface TaskRepository
+		extends JpaRepository<Task, Long>, CustomTaskRepository, BatchTaskRepository {
 
 	@Query(
 			"select t from Task t left join fetch t.category where t.calendar.id = :calendarId and"
