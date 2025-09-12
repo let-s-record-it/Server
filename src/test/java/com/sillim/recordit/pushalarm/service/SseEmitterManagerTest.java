6f292ebfe9c9e58c38e197affecdb2587a608d79
rt3310 <btac3310@gmail.com>
2025-09-13 00:11:31 +0900

    fix[#320]: 데이터 전송 실패 시 SseEmitter 삭제 및 완료 처리

--- src/test/java/com/sillim/recordit/pushalarm/service/SseEmitterManagerTest.java
+++ src/test/java/com/sillim/recordit/pushalarm/service/SseEmitterManagerTest.java
@@ -4,7 +4,6 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 import com.sillim.recordit.pushalarm.dto.AlarmType;
 import com.sillim.recordit.pushalarm.dto.PushMessage;
-import java.io.IOException;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -14,7 +13,7 @@ class SseEmitterManagerTest {
 
 	@Test
 	@DisplayName("sseEmitter로 client에게 message를 보낼 수 있다.")
-	void sendToClient() throws IOException {
+	void sendToClient() {
 		long memberId = 1L;
 		sseEmitterManager.subscribe(memberId);
 
@@ -27,7 +26,7 @@ class SseEmitterManagerTest {
 
 	@Test
 	@DisplayName("subscribe한 client가 없으면 message를 보낼 수 없다.")
-	void cantSendToClientIfNotExistsClientSubscribeSseEmitter() throws IOException {
+	void cantSendToClientIfNotExistsClientSubscribeSseEmitter() {
 		long memberId = 1L;
 
 		boolean result =
