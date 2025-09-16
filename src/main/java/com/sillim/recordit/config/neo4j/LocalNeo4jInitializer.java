b904e5dd06a7ff2ae0ddd39b70b166340e42bed4
sypark <soun997@gmail.com>
2025-09-17 02:44:19 +0900

    chore[#331]: neo4j initializer 설명 주석 추가

--- src/main/java/com/sillim/recordit/config/neo4j/LocalNeo4jInitializer.java
+++ src/main/java/com/sillim/recordit/config/neo4j/LocalNeo4jInitializer.java
@@ -8,17 +8,21 @@ import org.springframework.boot.ApplicationRunner;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Component;
 
+/// <summary>
+/// (Local 환경 한정) Spring Application 실행 시 Neo4j DB 데이터를 초기화
+/// </summary>
 @Component
 @RequiredArgsConstructor
 @Profile("local")
 public class LocalNeo4jInitializer implements ApplicationRunner {
 
+	private static final String INIT_QUERY = "MATCH (n) DETACH DELETE n";
 	private final Driver neo4jDriver;
 
 	@Override
 	public void run(ApplicationArguments args) {
 		try (Session session = neo4jDriver.session()) {
-			session.run("MATCH (n) DETACH DELETE n");
+			session.run(INIT_QUERY);
 		}
 	}
 }
