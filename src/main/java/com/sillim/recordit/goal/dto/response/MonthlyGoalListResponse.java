1726241fed0c3cba5e9d63712b72835b2b0e6b36
sypark <soun997@gmail.com>
2025-10-15 15:27:15 +0900

    fix[#342]: goal 조회 api 응답 필드 변경

--- src/main/java/com/sillim/recordit/goal/dto/response/MonthlyGoalListResponse.java
+++ src/main/java/com/sillim/recordit/goal/dto/response/MonthlyGoalListResponse.java
@@ -2,22 +2,14 @@ package com.sillim.recordit.goal.dto.response;
 
 import com.sillim.recordit.goal.domain.MonthlyGoal;
 
-public record MonthlyGoalListResponse(
-		Long id,
-		String title,
-		Long categoryId,
-		String colorHex,
-		Boolean achieved,
-		Long calendarId) {
+public record MonthlyGoalListResponse(Long id, String title, String colorHex, Boolean achieved) {
 
 	public static MonthlyGoalListResponse from(final MonthlyGoal monthlyGoal) {
 
 		return new MonthlyGoalListResponse(
 				monthlyGoal.getId(),
 				monthlyGoal.getTitle(),
-				monthlyGoal.getCategory().getId(),
 				monthlyGoal.getColorHex(),
-				monthlyGoal.isAchieved(),
-				monthlyGoal.getCalendar().getId());
+				monthlyGoal.isAchieved());
 	}
 }
